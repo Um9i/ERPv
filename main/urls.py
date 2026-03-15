6c9f372d026b89d25f5f40db3cead341361d1870
Um9i <vulgarmessiah@gmail.com>
2026-03-15 20:01:32 +0000

    feat(templates): add robots.txt and sitemap.xml for SEO optimization; update home page metadata

--- main/urls.py
+++ main/urls.py
@@ -32,6 +32,16 @@ urlpatterns = (
     ([path("admin/", admin.site.urls)] if settings.DEBUG else [])
     + [
         path("healthz/", HealthCheckView.as_view(), name="healthz"),
+        path(
+            "robots.txt",
+            TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+        ),
+        path(
+            "sitemap.xml",
+            TemplateView.as_view(
+                template_name="sitemap.xml", content_type="application/xml"
+            ),
+        ),
         path("accounts/", include("django_registration.backends.one_step.urls")),
         path(
             "accounts/login/",
